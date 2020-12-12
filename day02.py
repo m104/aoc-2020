1958aae2408b41d22e12ce61ecf1ab879679dffd
Adam Collins <adam@m104.us>
2020-12-12 11:56:51 -0800

    Day 2 code cleanup

--- day02.py
+++ day02.py
@@ -10,55 +10,61 @@ for line in sys.stdin:
     lines.append(line.strip())
 
 
-def parse_old_policy(line):
-    policy_str, letter_str, password = line.split(' ')
+def parse_first_policy(line):
+    policy_str, letter_str, password = line.split(" ")
     letter = letter_str[0]
-    low, high = tuple(map(int, policy_str.split('-')))
+    low, high = tuple(map(int, policy_str.split("-")))
     policy = range(low, high + 1)
     return (password, letter, policy)
 
 
-def validate_old_policy(password, letter, policy):
+def validate_first_policy(password, letter, policy):
     count = len(list(filter(lambda c: c == letter, password)))
     verdict = count in policy
-    print(f"verdict={verdict} letter={letter} count={count} policy={policy} password={password}")
+    print(
+        f"verdict={verdict} letter={letter} count={count} policy={policy} password={password}"
+    )
     return verdict
 
 
-def count_old_valid_passwords(lines):
+def count_first_valid_passwords(lines):
     def _validate_line(line):
-        password, letter, policy = parse_old_policy(line)
-        return validate_old_policy(password, letter, policy)
+        password, letter, policy = parse_first_policy(line)
+        return validate_first_policy(password, letter, policy)
+
     valid = filter(_validate_line, lines)
     return len(list(valid))
 
 
-answer = count_old_valid_passwords(lines)
-print(f"Part I Answer: {answer}")
-
-def parse_policy(line):
-    policy_str, letter_str, password = line.split(' ')
+def parse_second_policy(line):
+    policy_str, letter_str, password = line.split(" ")
     letter = letter_str[0]
-    low, high = tuple(map(int, policy_str.split('-')))
+    low, high = tuple(map(int, policy_str.split("-")))
     policy = (low - 1, high - 1)
     return (password, letter, policy)
 
 
-def validate_policy(password, letter, indexes):
+def validate_second_policy(password, letter, indexes):
     valid = filter(lambda index: password[index] == letter, indexes)
     count = len(list(valid))
     verdict = count == 1
-    print(f"verdict={verdict} letter={letter} count={count} indexes={indexes} password={password}")
+    print(
+        f"verdict={verdict} letter={letter} count={count} indexes={indexes} password={password}"
+    )
     return verdict
 
 
-def count_valid_passwords(lines):
+def count_second_valid_passwords(lines):
     def _validate_line(line):
-        password, letter, policy = parse_policy(line)
-        return validate_policy(password, letter, policy)
+        password, letter, policy = parse_second_policy(line)
+        return validate_second_policy(password, letter, policy)
+
     valid = filter(_validate_line, lines)
     return len(list(valid))
 
 
-answer = count_valid_passwords(lines)
+answer = count_first_valid_passwords(lines)
+print(f"Part I Answer: {answer}")
+
+answer = count_second_valid_passwords(lines)
 print(f"Part II Answer: {answer}")
