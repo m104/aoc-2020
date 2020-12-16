424857a23d9ad0e1fd57b965083ec77952bebc3c
Adam Collins <adam@m104.us>
2020-12-15 20:44:17 -0800

    Add convenience template

---
+++ template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+# Debug:
+#   ./dayXX.py < dayXX.input.txt
+# Run:
+#   ./dayXX.py < dayXX.input.txt 2>/dev/null
+
+import sys
+
+input_lines = []
+for line in sys.stdin:
+    input_lines.append(line.strip())
+
+answer = None
+print(f"Part I Answer: {answer}")
+
+answer = None
+print(f"Part II Answer: {answer}")
